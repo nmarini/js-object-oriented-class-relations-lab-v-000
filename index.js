ab8f15a581014c36c02d11173b5f64fb386c419b
Nicolas Marini <nmarini44@gmail.com>
2019-05-02 23:02:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+let driverId = 0;
+ 
+class Driver {
+    constructor(name) {
+        this.id = ++driverId;
+        this.name = name;
+    }
+}
+ 
+let passengerId = 0;
+ 
+class Passenger {
+    constructor(name) {
+        this.id = ++passengerId;
+        this.name = name;
+        if (driver) {
+            this.driverId = driver.id;
+        }
+
+        store.items.push(this);
+    }
+    setUser(user) {
+        this.userId = user.id;
+    }
+}
+
+let tripId = 0
+class Trip {
+  constructor(driver, passenger){
+    this.id = ++tripId;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id
+  }
+}
+ 
