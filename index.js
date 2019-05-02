9a921218b9e3e4fe7ac0eae171be3db78b0cd351
Nicolas Marini <nmarini44@gmail.com>
2019-05-02 23:32:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class Driver {
     }
 
     passengers(){
-      return this.trips().filter(
+      return this.trips().map(
         function(trip){
           return trip.passenger();
         }
