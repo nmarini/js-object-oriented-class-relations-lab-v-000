235bf60ce5396e6d9fb1eacb3c2dd60a1b57a93d
Nicolas Marini <nmarini44@gmail.com>
2019-05-02 23:27:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,13 +48,13 @@ class Trip {
 
     store.trips.push(this);
   }
-  // passenger(){
-  //   return store.passengers.find(
-  //     function(passenger){
-  //       return passenger.id === this.passengerId
-  //     }
-  //   )
-  // }.bind(this)
+  passenger(){
+    return store.passengers.find(
+      function(passenger){
+        return passenger.id === this.passengerId
+      }.bind(this)
+    )
+  }
 
 
 }
