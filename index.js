19db2265b3baf25673a96b8beb7ff827b977e896
Nicolas Marini <nmarini44@gmail.com>
2019-05-02 23:02:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let driverId = 0;
- 
+
 class Driver {
     constructor(name) {
         this.id = ++driverId;
         this.name = name;
     }
 }
- 
+
 let passengerId = 0;
- 
+
 class Passenger {
     constructor(name) {
         this.id = ++passengerId;
